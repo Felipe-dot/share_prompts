3c5da4fc67e5ad4c39548ad96603fd74bd2a56f8
Felipe-dot <felipeluis610@gmail.com>
2024-01-26 15:11:29 -0300

    Wrap provider component in root layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,13 +12,15 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient"></div>
-        </div>
-        <div className="app">
-          <Nav />
-          {children}
-        </div>
+        <Provider>
+          <div className="main">
+            <div className="gradient"></div>
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
