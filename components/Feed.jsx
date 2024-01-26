a22bd2fef7a54b4ff32a50c13ca3e9a863096838
Felipe-dot <felipeluis610@gmail.com>
2024-01-26 15:12:19 -0300

    Fix property name

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -5,11 +5,14 @@ import { useState, useEffect } from "react";
 import PromptCard from "./PromptCard";
 
 const PromptCardList = ({ data, handleTagClick }) => {
-  console.log(data);
   return (
     <div className="mt-16 prompt_layout">
       {data.map((post) => (
-        <PromptCard key={post.id} post={post} handleTagClick={handleTagClick} />
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
       ))}
     </div>
   );
